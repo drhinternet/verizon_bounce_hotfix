5ab5ebf51389d8fae16e33911689efb4bca7b923
Alex McHale <alex@anticlever.com>
2014-10-24 16:04:38 -0500

    first stab at run script

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ "$UID" -ne 0 ]; then
+  echo "this must be run as root"
+  exit 1
+fi
+
+working_dir="/root/verizon_bounce_hotfix"
+engine_script="$working_dir/reactivate-engine-bounces.sh"
+studio_script="$working_dir/reactivate-studio-bounces.rb"
+
+mkdir -p $working_dir
+rm -rf $working_dir/*
+
+if [ -d "/var/hvmail" ]; then
+  curl https://raw.githubusercontent.com/drhinternet/verizon_bounce_hotfix/master/reactivate-engine-bounces.sh > $engine_script
+  chmod u+x $engine_script
+  $engine_script
+fi
+
+if [ -d "/var/hvmail/studio" ]; then
+  cd /var/hvmail/studio
+
+  curl https://raw.githubusercontent.com/drhinternet/verizon_bounce_hotfix/master/reactivate-studio-bounces.rb > $studio_script
+  chmod u+x $studio_script
+
+  $studio_script                                 \
+    --no-dry-run                                 \
+    --require-start-time '2014-10-20 18:00 CDT'  \
+    --require-end-time '2014-10-21 21:30 CDT'    \
+    --require-bounce-text-sql-like '%550%alias%' \
+    --require-domain 'verizon.net'
+fi

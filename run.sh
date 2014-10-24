3b452d255260f14fc9d015daf17ff22eea428b00
Alex McHale <alex@anticlever.com>
2014-10-24 16:08:47 -0500

    move studio script into place, it assumes its pwd

--- run.sh
+++ run.sh
@@ -10,7 +10,6 @@ engine_script="$working_dir/reactivate-engine-bounces.sh"
 studio_script="$working_dir/reactivate-studio-bounces.rb"
 
 mkdir -p $working_dir
-rm -rf $working_dir/*
 
 if [ -d "/var/hvmail" ]; then
   curl https://raw.githubusercontent.com/drhinternet/verizon_bounce_hotfix/master/reactivate-engine-bounces.sh > $engine_script
@@ -19,15 +18,18 @@ if [ -d "/var/hvmail" ]; then
 fi
 
 if [ -d "/var/hvmail/studio" ]; then
-  cd /var/hvmail/studio
-
   curl https://raw.githubusercontent.com/drhinternet/verizon_bounce_hotfix/master/reactivate-studio-bounces.rb > $studio_script
   chmod u+x $studio_script
 
-  $studio_script                                 \
+  cd /var/hvmail/studio
+  cp $studio_script script/reactivate-bounced-subscribers
+
+  script/reactivate-bounced-subscribers          \
     --no-dry-run                                 \
     --require-start-time '2014-10-20 18:00 CDT'  \
     --require-end-time '2014-10-21 21:30 CDT'    \
     --require-bounce-text-sql-like '%550%alias%' \
     --require-domain 'verizon.net'
 fi
+
+rm -rf $working_dir
